ff3600cf0811b9139fdb02041efc2cbfdb4ecf30
rajesh8179 <36216174+rajesh8179@users.noreply.github.com>
2019-07-08 14:33:42 +0530

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -1,5 +1,5 @@
 public class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Java");  
+     System.out.println("Hello Java im very happy to see like this");  
     }  
 } 
